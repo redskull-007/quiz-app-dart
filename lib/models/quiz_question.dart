a4c3e282a0972fbda03a081140bee9ccad7669ec
Kumar Abhishek <ka61992@gmail.com>
2024-09-25 12:23:57 +0530

    Created reusable customed style button,used mapping and spread operator and used google fonts

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffuledList = List.of(answers);
+    shuffuledList.shuffle();
+    return shuffuledList;
+  }
 }
